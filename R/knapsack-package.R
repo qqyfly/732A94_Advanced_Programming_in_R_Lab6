9e2c9cba13ff0db9513b7b15e658cd756ba7ea64
sakaushikb4u <76866642+sakaushikb4u@users.noreply.github.com>
2023-10-16 14:30:07 +0200

    init project structure

---
+++ R/knapsack-package.R
@@ -0,0 +1,11 @@
+#' Lab 6 of Advanced Programming in R
+#'
+#' This package include following functions:\cr
+#' 1: brute_force_knapsack(x, W, fast = FALSE, parallel = FALSE)\cr 
+#' 2: knapsack_dynamic(x, W, fast = FALSE)\cr
+#' 3: greedy_knapsack(x, W, fast = FALSE)\cr
+
+#' @name knapsack-package
+#' @aliases knapsack
+#' @docType package
+NULL
